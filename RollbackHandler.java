720f3197097c8abacf36f40065db0ae49caade26
KingoZ <44167391+KingoZ@users.noreply.github.com>
2018-12-01 14:06:51 -0200

    Update RollbackHandler.java

--- RollbackHandler.java
+++ RollbackHandler.java
@@ -62,7 +62,7 @@ public class RollbackHandler {
 				try {
 					rollback(worldName);
 				} catch (Exception e) {
-					Bukkit.getConsoleSender().sendMessage("§cErro ao tentar criar o mapa.");
+					Bukkit.getConsoleSender().sendMessage("Â§cErro ao tentar criar o mapa.");
 				}
 				
 				new BukkitRunnable() {
@@ -115,33 +115,26 @@ public class RollbackHandler {
 	private void copyFolder(File src, File dest) throws IOException {
 		if (src.isDirectory()) {
 
-			// if directory not exists, create it
 			if (!dest.exists()) {
 				dest.mkdir();
 			}
 
-			// list all the directory contents
 			String files[] = src.list();
 
 			if (files != null) {
 				for (String file : files) {
-					// construct the src and dest file structure
 					File srcFile = new File(src, file);
 					File destFile = new File(dest, file);
-					// recursive copy
 					copyFolder(srcFile, destFile);
 				}
 			}
 		} else {
-			// if file, then copy it
-			// Use byte stream to support all file types
 			InputStream in = new FileInputStream(src);
 			OutputStream out = new FileOutputStream(dest);
 
 			byte[] buffer = new byte[1024];
 
 			int length;
-			// copy the file content in bytes
 			while ((length = in.read(buffer)) > 0) {
 				out.write(buffer, 0, length);
 			}
